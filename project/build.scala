7f60562ee5cdb4235d1d8b6daf45d931b7c042b8
theclaymethod <clayton.kim@gmail.com>
2015-03-21 09:38:23 -0700

    Bump Version Numbers
    Spark, SBT, and various plugins

--- project/build.scala
+++ project/build.scala
@@ -8,11 +8,17 @@ import sbtassembly.Plugin._
 import AssemblyKeys._
 
 object spark_ml extends Build {
+  
+  object V {
+    val Spark = "1.3.0"
+    val Hadoop = "1.0.4"
+  }
+  
   lazy val sharedLibraryDependencies = Seq(
     "org.scalatest" %% "scalatest" % "2.1.5" % "test",
-    "org.apache.spark" %% "spark-core" % "1.0.1" % "provided",
-    "org.apache.spark" %% "spark-mllib" % "1.0.1" % "provided",
-    "org.apache.hadoop" % "hadoop-client" % "1.0.4" % "provided",
+    "org.apache.spark" %% "spark-core" % V.Spark % "provided",
+    "org.apache.spark" %% "spark-mllib" % V.Spark % "provided",
+    "org.apache.hadoop" % "hadoop-client" % V.Hadoop % "provided",
     "org.spire-math" %% "spire" % "0.8.2",
     "org.scalanlp" %% "breeze" % "0.8" % "provided",
     "com.github.scopt" %% "scopt" % "3.2.0"
